5c82e53a94c129b59bace908700c70ea8fcf7204
ashiq <ashiq@mvp-apps.ae>
2025-05-17 16:41:41 +0400

    refactor: reorganize exports in voice_message_package for better structure

--- lib/voice_message_package.dart
+++ lib/voice_message_package.dart
@@ -1,5 +1,11 @@
 library voice_message_package;
 
+/// Helpers
+export 'src/helpers/play_duration.dart';
+
+/// Views
 export 'src/line_voice_message_view.dart';
+
+/// Controllers
 export 'src/voice_controller.dart';
 export 'src/voice_message_view.dart';
